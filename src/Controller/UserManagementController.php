01dfb9875ff3e9658d2bee7008d0b1945c6bad29
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2025-12-24 17:11:55 +0100

    Add crud User

---
+++ src/Controller/UserManagementController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/Accueil/Utilisateurs/Gestion')]
+final class UserManagementController extends AbstractController
+{
+    #[Route(name: 'gestion_equipe_user_management_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user_management/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/Ajouter', name: 'gestion_equipe_user_management_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+
+            if($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            }
+            
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Confirmation : nouvel utilisateur ' . $user->getFirstName() . ' ' . $user->getLastName() . ' ajouté avec succès.');
+
+            return $this->redirectToRoute('gestion_equipe_user_management_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user_management/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/Consulter', name: 'gestion_equipe_user_management_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user_management/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/Modifier', name: 'gestion_equipe_user_management_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Confirmation : les informations de l\'utilisateur ' 
+                . $user->getFirstName() . ' ' . $user->getLastName() . ' ont été mises à jour avec succès.');
+
+            return $this->redirectToRoute('gestion_equipe_user_management_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user_management/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/Supprimer', name: 'gestion_equipe_user_management_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Confirmation : l\'utilisateur ' . $user->getFirstName() . ' ' . $user->getLastName() . ' supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Erreur : jeton CSRF invalide. La suppression de l\'utilisateur a échoué.');
+        }
+
+        return $this->redirectToRoute('app_user_management_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email professionnel',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+                'constraints' => [
+                        new NotBlank(message: 'Veuillez saisir un mot de passe.'),
+                        new Length(
+                            min: 8,
+                            max: 4096,
+                            minMessage: 'Votre mot de passe doit comporter au moins {{ limit }} caractères.'
+                        ),
+                        new Regex(
+                            pattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/',
+                            message: 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.'
+                        ),
+                        new NotCompromisedPassword(
+                            message: 'Ce mot de passe a été compromis lors d’une fuite de données. Veuillez en choisir un autre.'
+                        ),
+                    ],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('emailPrivate', EmailType::class, [
+                'label' => 'Email personnel',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('phonePro', TextType::class, [
+                'label' => 'Téléphone pro',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('companyId', TextType::class, [
+                'label' => 'ID entreprise',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('adress', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('position', TextType::class, [
+                'label' => 'Poste',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('department', TextType::class, [
+                'label' => 'Département',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('avatar', TextType::class, [
+                'label' => 'Avatar (URL)',
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+            ])
+            ->add('isActive', CheckboxType::class, [
+                'label' => 'Actif',
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
