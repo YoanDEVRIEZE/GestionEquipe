58459c0a1870d593de8cd38beefa6ef0966e8846
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2026-01-06 09:04:53 +0100

    Fix error empty data DataTable

--- public/js/dataTable.js
+++ public/js/dataTable.js
@@ -1,23 +1,23 @@
-if ($('#dataTable tbody tr').length > 0) {
-    $(document).ready(function () {
-        var table = $('#dataTable').DataTable({
-            paging: true,
-            ordering: true,
-            info: false,
-            pageLength: 11,
-            columnDefs: [
-                { orderable: false, targets: -1 }
-            ],
-            language: {
-                url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json'
-            },
-            dom: 'ft<"d-flex justify-content-end mt-4"p>',
-            initComplete: function () {
-                $('#dataTable_filter').prependTo('#datatable-controls');
-                $('#dataTable_filter').addClass('d-flex align-items-center mb-0 text-black').css('margin-top', '-20px');
-                $('#dataTable_filter input').addClass('form-control form-control-sm me-2 text-black').css('max-width', '150px').css('border', '1px solid black');
-                $('#dataTable_filter label').addClass('mb-0 text-black');
-            }
-        });
+
+$(document).ready(function () {
+    var table = $('#dataTable').DataTable({
+        paging: true,
+        ordering: true,
+        info: false,
+        pageLength: 11,
+        columnDefs: [
+            { orderable: false, targets: -1 }
+        ],
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json',
+            emptyTable: 'Aucun utilisateur trouvé',
+        },
+        dom: 'ft<"d-flex justify-content-end mt-4"p>',
+        initComplete: function () {
+            $('#dataTable_filter').prependTo('#datatable-controls');
+            $('#dataTable_filter').addClass('d-flex align-items-center mb-0 text-black').css('margin-top', '-20px');
+            $('#dataTable_filter input').addClass('form-control form-control-sm me-2 text-black').css('max-width', '150px').css('border', '1px solid black');
+            $('#dataTable_filter label').addClass('mb-0 text-black');
+        }
     });
-}
+});
