6533df4819f96aa2261f96006f51407df4c8b69c
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2026-01-06 22:22:33 +0100

    Fix dataTable responsive

--- public/js/dataTable.js
+++ public/js/dataTable.js
@@ -1,10 +1,13 @@
+let table;
 
 $(document).ready(function () {
-    var table = $('#dataTable').DataTable({
+    table = $('#dataTable').DataTable({
         paging: true,
         ordering: true,
         info: false,
         pageLength: 11,
+        responsive: true,
+        autoWidth: false,
         columnDefs: [
             { orderable: false, targets: -1 }
         ],
@@ -15,9 +18,25 @@ $(document).ready(function () {
         dom: 'ft<"d-flex justify-content-end mt-4"p>',
         initComplete: function () {
             $('#dataTable_filter').prependTo('#datatable-controls');
-            $('#dataTable_filter').addClass('d-flex align-items-center mb-0 text-black').css('margin-top', '-20px');
-            $('#dataTable_filter input').addClass('form-control form-control-sm me-2 text-black').css('max-width', '150px').css('border', '1px solid black');
+            $('#dataTable_filter')
+                .addClass('d-flex align-items-center mb-0 text-black')
+                .css('margin-top', '-20px');
+
+            $('#dataTable_filter input')
+                .addClass('form-control form-control-sm me-2 text-black')
+                .css({
+                    maxWidth: '150px',
+                    border: '1px solid black'
+                });
+
             $('#dataTable_filter label').addClass('mb-0 text-black');
         }
     });
 });
+
+$(window).on('resize', function () {
+    if (table) {
+        table.columns.adjust().draw();
+    }
+});
+
